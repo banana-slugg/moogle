5603c1864cf7500737e68486f1a34fa266ca2abf
crims1n <andrew.boram@hotmail.com>
2024-01-23 17:05:59 -0500

    fix: remove whitespace from episode description

--- pkg/podbean/podbean.go
+++ pkg/podbean/podbean.go
@@ -1,7 +1,9 @@
 package podbean
 
 import (
+	"fmt"
 	"html/template"
+	"strings"
 
 	"github.com/mmcdole/gofeed"
 )
@@ -18,18 +20,22 @@ func GetPodbean() (*PodBeanInfo, error) {
 		Description: feed.Description,
 		Link:        feed.Link,
 		Copyright:   feed.Copyright,
+		Logo:        feed.Image.URL,
 	}
 
 	for _, item := range feed.Items {
+		description := item.Description
+		description = strings.ReplaceAll(description, "<p>\u00a0</p>", "")
+		description = strings.TrimSpace(description)
+		fmt.Println(description)
 		info.Episodes = append(info.Episodes, Episode{
 			Title:       item.Title,
-			Description: template.HTML(item.Description),
+			Description: template.HTML(description),
 			Published:   item.PublishedParsed,
 			Link:        item.Link,
 			DirectLink:  item.Enclosures[0].URL,
 		})
 
 	}
-
 	return info, nil
 }
